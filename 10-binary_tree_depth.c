a1565fa9e2177f840bb4660c496f079fe3cb02ef
GideonMissoy <giddykips2030@gmail.com>
2023-08-04 19:24:06 -0700

    10. Depth. Function measures the depth of anode in abinary tree.

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: pointer to the node to measure the depth
+ * Return: depth of the node, or 0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
